fa6820afeecdc04e698553a9dd1575fe16f40168
Filip Norberg <45309184+filipnorberg@users.noreply.github.com>
2021-09-25 16:28:57 +0200

    tested cmath

---
+++ math.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+main() {
+    cout << max(5, 10) << "\n";
+    cout << min(5, 10) << "\n";
+    cout << sqrt(64) << "\n";
+    cout << round(2.4) << "\n";
+    cout << log(2) << "\n";
+}
