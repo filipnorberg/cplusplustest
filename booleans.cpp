0264bd4c644f04372a7eb01332732be8d81c40c6
Filip Norberg <45309184+filipnorberg@users.noreply.github.com>
2021-09-25 16:28:44 +0200

    tested booleans

---
+++ booleans.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x = 10;
+    int y = 9;
+    bool isCodingFun = true;
+    bool isCodingEasy = false;
+    cout << isCodingFun << "\n";
+    cout << isCodingEasy << "\n";
+    cout << (x < y) << "\n";
+    cout << (x == 10) << "\n";
+    cout << (x == 15);
+}
