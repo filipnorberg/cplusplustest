a173242ae17d09d8b998592aef2887eb0a0736c9
Filip Norberg <45309184+filipnorberg@users.noreply.github.com>
2021-09-25 14:51:41 +0200

    made a simple c++ program with variables

---
+++ first.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+//Function that types out text
+int main() {
+    string myName, myProfession;
+    int myAge;
+
+    myAge = 19;
+    myName = "Filip Norberg";
+    myProfession = "Programming";
+
+    cout << "Hello, my name is " << myName << ". I am " << myAge << " years old and i work with " << myProfession;
+    return 0;
+}
