d239fad4024aaee087b2cf35f04c3de19de556ce
Filip Norberg <45309184+filipnorberg@users.noreply.github.com>
2021-09-25 18:07:28 +0200

    tested switch function

---
+++ switch.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int day = 4;
+    switch (day) {
+        
+        case 6: 
+          cout << "Saturday";
+          break;
+        case 7: 
+          cout << "Sunday";
+          break;
+        default:
+          cout << "Looking forward to the weekend!";
+    }
+}
